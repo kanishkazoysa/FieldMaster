63a0d24ec083e8079b1c35844c3007a722032b71
kanishkazoysa <125685188+kanishkazoysa@users.noreply.github.com>
2024-02-02 05:01:05 +0530

    FM | add google sign

--- MobileApp/app/src/screens/RegisterScreen.js
+++ MobileApp/app/src/screens/RegisterScreen.js
@@ -1,4 +1,9 @@
 import React, { useState } from "react";
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
 import {
   View,
   Text,
@@ -19,14 +24,61 @@ import {
   responsiveFontSize,
 } from "react-native-responsive-dimensions";
 
+
+WebBrowser.maybeCompleteAuthSession();
+
 export default function RegisterScreen() {
-  
+  const [userInfo, setUserInfo] = React.useState(null);
+  const [request, response, promptAsync] = Google.useAuthRequest({
+  androidClientId:"1002701216584-p77alfohcrqcd3seea2ht2ll1sk4knug.apps.googleusercontent.com",
+  iosClientId:"1002701216584-p77alfohcrqcd3seea2ht2ll1sk4knug.apps.googleusercontent.com",
+  webClientId:"1002701216584-e9rmdbv6rk7a0ffg9c6v55sl697v5r5m.apps.googleusercontent.com"
+  });
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const navigation = useNavigation();
+
+  React.useEffect(() => {
+    handleSignInWithGoogle();
+  }, [response]);
+
+
+  async function handleSignInWithGoogle() {
+    const user = await AsyncStorage.getItem("@user");
+    if (!user) {
+      if (response?.type === 'success') {
+      await getUserInfo(response.authentication.accessToken);
+      }
+    }
+    else {
+      setUserInfo(JSON.parse(user));
+    }
+  }
+
+  const getUserInfo = async (token) => {
+    if (!token) return;
+    try {
+      const response = await fetch("https://www.googleapis.com/userinfo/v2/me", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+
+      const user = await response.json();
+      await AsyncStorage.setItem("@user", JSON.stringify(user));
+      setUserInfo(user);
+    } catch (error) {
+      
+    }
+  };
+
+
+
+
+
+
+
 
   const handleSignUp = async () => {
     try {
@@ -137,6 +189,14 @@ export default function RegisterScreen() {
         <View style={styles.line}></View>
       </View>
 
+     <View style={styles.userInfo}>
+     <Text style={{fontSize:responsiveFontSize(1)}}>{JSON.stringify(userInfo , null ,2 )}</Text>
+     </View>
+
+      <View style={styles.Googlebutton}>
+          <Button title="GOOGLE SIGN IN" onPress={ () => promptAsync()} />
+      </View>
+
       <View style={styles.privacyTermsContainer}>
         <Text style={styles.privacyText}>
           By clicking "Sign up" you agree to our
@@ -158,6 +218,16 @@ export default function RegisterScreen() {
 }
 
 const styles = StyleSheet.create({
+
+  userInfo: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: responsiveHeight(5),
+  },
+  Googlebutton: {
+    top: responsiveHeight(5),
+    alignItems: "center",
+  },
   header: {
     fontSize: responsiveFontSize(5),
     fontWeight: "bold",
