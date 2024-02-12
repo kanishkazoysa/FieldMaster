dabb126b19cb1c961c5f1cfd3cebb1c7fcbc26f7
kanishkazoysa <125685188+kanishkazoysa@users.noreply.github.com>
2024-02-12 12:20:20 +0530

    FM | setup web routes

---
+++ WebApp/client/src/pages/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from "../componenets/Header";
+
+export default function Contact() {
+    return (
+      <div>
+        <Header />
+        <h1>Contact Page</h1>
+      </div>
+    );
+  }
