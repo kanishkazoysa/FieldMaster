067db380573690c91f09feb037f7cac4d7c35250
hirushan2001 <135137760+hirushan2001@users.noreply.github.com>
2024-03-02 18:59:43 +0530

    FM |  fence route

--- WebApp/routes/fenceRoute.js
+++ WebApp/routes/fenceRoute.js
@@ -1,3 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const fenceModel = require("../models/fence");
+
+
+router.post("/fence", async (req, res) => {
+    const { FenceType, PostSpace, PostSpaceUnit, Gatelength, NumberofGates } = req.body;
+
+    try {
+        
+    }
+})
+
+
+module.exports = router;
