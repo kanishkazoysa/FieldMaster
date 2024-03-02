43e4c5e4c1424a5519cede6488fbebf0e3f43c6c
hirushan2001 <135137760+hirushan2001@users.noreply.github.com>
2024-03-02 13:49:32 +0530

    FM | backend  setup

---
+++ WebApp/models/fence.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const fenceSchema = mongoose.Schema(
+    {
+        FenceType: {
+            type: String,
+        },
+        PostSpace: {
+            type: String,
+        },
+        PostSpaceUnit: {
+            type: String,
+        },
+        Gatelength: {
+            type: String,
+        },
+        NumberofGates: {
+            type: String,
+        },
+    }
+); 
+
+const fenceModel = mongoose.model("fence", fenceSchema);
+module.exports = fenceModel;       
