9f60e012c9690750cc9b9042bb6c45b586457e8f
Shenali Gamage <87947238+Nethmi-Shenali@users.noreply.github.com>
2024-03-26 20:04:55 +0530

    FM | fertilizer added to server

--- WebApp/server.js
+++ WebApp/server.js
@@ -11,6 +11,7 @@ const dbconfig = require("./db");
 const userRoute = require("./routes/usersRoute.js");
 const mailRoute = require("./routes/mailRoute.js");
 const plantationRoute = require("./routes/plantationRoute.js");
+const fertilizerRoute= require("./routes/fertilizerRoute.js");
 // const fenceRoute = require("./routes/fenceRoute.js");
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
@@ -20,6 +21,7 @@ app.use(express.json());
 app.use("/api/users", userRoute);
 app.use("/api/mail", mailRoute);
 app.use("/api/plantation", plantationRoute);
+app.use("/api/fertilizer", fertilizerRoute);
 // app.use("/api/fence", fenceRoute);
 
 const port = process.env.PORT || 5000;
