7a705337e993e6cad4f373280ad4a27d3dcc6003
Shenali Gamage <87947238+Nethmi-Shenali@users.noreply.github.com>
2024-03-26 18:53:32 +0530

    FM | fertilizer model created

---
+++ WebApp/models/fertilizer.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const fertilizerSchema = mongoose.Schema(
+    {
+        TimePeriod: {
+            type: String,
+        },
+        FertilizerType: {
+            type: String,
+        },
+        NoOfTimes: {
+            type: String,
+        },
+        AmountOfOneTime: {
+            type: String,
+        }
+    }
+); 
+
+const fertilizerModel = mongoose.model("fertilizer", fertilizerSchema);
+module.exports = fertilizerModel;       
