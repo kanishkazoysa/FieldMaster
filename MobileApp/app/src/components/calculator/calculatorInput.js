ea0122adb592ca6882b47c701fba6672d1d4749e
kanishkazoysa <125685188+kanishkazoysa@users.noreply.github.com>
2024-04-19 13:37:53 +0530

    FM | edit keyboard

--- MobileApp/app/src/components/calculator/calculatorInput.js
+++ MobileApp/app/src/components/calculator/calculatorInput.js
@@ -6,7 +6,7 @@ import {
   StyleSheet,
   TextInput,
   Modal,
-  KeyboardAvoidingView,
+  ScrollView,
 } from "react-native";
 import { IconButton, Button } from "react-native-paper";
 import { useNavigation } from "@react-navigation/native";
@@ -19,6 +19,7 @@ import {
   responsiveWidth,
   responsiveFontSize,
 } from "react-native-responsive-dimensions";
+import { Platform } from "react-native";
 
 const CalculatorSelectModel = ({
   calculatorModalVisible,
@@ -53,101 +54,118 @@ const CalculatorSelectModel = ({
             activeOpacity={1}
             onPress={closeModal}
           />
+          <ScrollView keyboardShouldPersistTaps="handled">
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <IconButton
+                  icon="close"
+                  iconColor="#000"
+                  size={responsiveFontSize(3.2)}
+                  onPress={closeModal}
+                  style={styles.cancelButton}
+                />
 
-          <View style={styles.centeredView}>
-            <View style={styles.modalView}>
-              <IconButton
-                icon="close"
-                iconColor="#000"
-                size={responsiveFontSize(3.2)}
-                onPress={closeModal}
-                style={styles.cancelButton}
-              />
+                <Text style={styles.headerText}>Manual Calculator</Text>
 
-              <Text style={styles.headerText}>Manual Calculator</Text>
-
-              <View style={styles.inputContainer}>
-                <View style={styles.iconContainer}>
-                  <AntDesign name="piechart" size={responsiveFontSize(3)} color="#65676B" />
-                  <Text style={{ fontSize: responsiveFontSize(1.9), marginLeft: responsiveWidth(2) }}>Area</Text>
-                </View>
-
-                <View style={styles.iconContainer}>
-                  <TextInput
-                    style={styles.input}
-                    placeholder={"00.00"}
-                    keyboardType="numeric"
-                  />
-
-                  <View style={styles.dropdown}>
-                    <Picker
-                      selectedValue={selectedValue}
-                      style={{ 
-                        width: responsiveWidth(30),
-                       
+                <View style={styles.inputContainer}>
+                  <View style={styles.iconContainer}>
+                    <AntDesign
+                      name="piechart"
+                      size={responsiveFontSize(3)}
+                      color="#65676B"
+                    />
+                    <Text
+                      style={{
+                        fontSize: responsiveFontSize(1.9),
+                        marginLeft: responsiveWidth(2),
                       }}
-                      onValueChange={(itemValue, itemIndex) =>
-                        setSelectedValue(itemValue)
-                      }
                     >
-                      <Picker.Item label="Square Meters" value="sqm" />
-                      <Picker.Item label="Square Feet" value="sqft" />
-                      <Picker.Item label="Acres" value="acres" />
-                      {/* Add more items as needed */}
-                    </Picker>
+                      Area
+                    </Text>
+                  </View>
+
+                  <View style={styles.iconContainer}>
+                    <TextInput
+                      style={styles.input}
+                      placeholder={"00.00"}
+                      keyboardType="numeric"
+                    />
+
+                    <View style={styles.dropdown}>
+                      <Picker
+                        selectedValue={selectedValue}
+                        style={{
+                          height: responsiveHeight(6),
+                          width: responsiveWidth(30),
+                        }}
+                        onValueChange={(itemValue, itemIndex) =>
+                          setSelectedValue(itemValue)
+                        }
+                      >
+                        <Picker.Item label="Square Meters" value="sqm" />
+                        <Picker.Item label="Square Feet" value="sqft" />
+                        <Picker.Item label="Acres" value="acres" />
+                        {/* Add more items as needed */}
+                      </Picker>
+                    </View>
                   </View>
                 </View>
-              </View>
 
-              <View style={styles.inputContainer}>
-                <View style={styles.iconContainer}>
-                  <MaterialCommunityIcons
-                    name="vector-rectangle"
-                    size={responsiveFontSize(3.4)}
-                    color="#65676B"
-                  />
-                  <Text style={{fontSize: responsiveFontSize(1.9), marginLeft: responsiveWidth(2)  }}>
-                    Perimeter
-                  </Text>
-                </View>
-
-                <View style={styles.iconContainer}>
-                  <TextInput
-                    style={styles.input}
-                    placeholder={"00.00"}
-                    keyboardType="numeric"
-                  />
-
-                  <View style={styles.dropdown}>
-                    <Picker
-                      selectedValue={selectedValue}
-                      style={{ 
-  
-                        width: responsiveWidth(30), 
+                <View style={styles.inputContainer}>
+                  <View style={styles.iconContainer}>
+                    <MaterialCommunityIcons
+                      name="vector-rectangle"
+                      size={responsiveFontSize(3.4)}
+                      color="#65676B"
+                    />
+                    <Text
+                      style={{
+                        fontSize: responsiveFontSize(1.9),
+                        marginLeft: responsiveWidth(2),
                       }}
-                      onValueChange={(itemValue, itemIndex) =>
-                        setSelectedValue(itemValue)
-                      }
                     >
-                      <Picker.Item label="m" value="m" />
-                      <Picker.Item label="Km" value="km" />
+                      Perimeter
+                    </Text>
+                  </View>
 
-                      {/* Add more items as needed */}
-                    </Picker>
+                  <View style={styles.iconContainer}>
+                    <TextInput
+                      style={styles.input}
+                      placeholder={"00.00"}
+                      keyboardType="numeric"
+                    />
+
+                    <View style={styles.dropdown}>
+                      <Picker
+                        selectedValue={selectedValue}
+                        style={{
+                          height: responsiveHeight(6),
+                          width: responsiveWidth(30),
+                        }}
+                        onValueChange={(itemValue, itemIndex) =>
+                          setSelectedValue(itemValue)
+                        }
+                      >
+                        <Picker.Item label="m" value="m" />
+                        <Picker.Item label="Km" value="km" />
+
+                        {/* Add more items as needed */}
+                      </Picker>
+                    </View>
                   </View>
                 </View>
-              </View>
 
-              <Button
-                mode="contained"
-                style={styles.calculateButton}
-                buttonColor="#007BFF"
-                onPress={handleCalculate}
-              >
-                Calculate
-              </Button>
+                <Button
+                  mode="contained"
+                  style={styles.calculateButton}
+                  buttonColor="#007BFF"
+                  onPress={handleCalculate}
+                >
+                  Calculate
+                </Button>
+              </View>
             </View>
-          </View>
+          </ScrollView>
         </View>
       </Modal>
       <CalculatorSelect
@@ -184,15 +202,16 @@ const styles = StyleSheet.create({
     borderBottomColor: "gray",
     borderBottomWidth: 1.5,
     paddingLeft: 10,
-    marginTop:responsiveHeight(0.5),
+    marginTop: responsiveHeight(0.5),
   },
 
   headerText: {
-    fontSize:responsiveFontSize(2),
+    fontSize: responsiveFontSize(2),
   },
   calculateButton: {
-    marginTop: responsiveHeight(25),
-    width:"85%"
+    position: "absolute", // This line
+    marginTop: responsiveHeight(49),
+    width: "85%",
   },
 
   container: {
@@ -203,7 +222,7 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   modalView: {
-    marginTop: responsiveHeight(30),
+    marginTop: responsiveHeight(40),
     alignSelf: "center",
     backgroundColor: "#fff",
     borderRadius: 25,
@@ -219,7 +238,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.4,
     shadowRadius: 4,
     width: "100%",
-    height: responsiveHeight(100),
+    height: responsiveHeight(60),
   },
 
   cancelButton: {
