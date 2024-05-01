1c42fe9e0a2bec0a02d4ca0a74a71b4fa8b045cd
kanishkazoysa <125685188+kanishkazoysa@users.noreply.github.com>
2024-05-01 23:12:27 +0530

    edit ip

--- MobileApp/app/src/AxiosInstance.js
+++ MobileApp/app/src/AxiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const AxiosInstance = axios.create({
-  baseURL: "http://10.10.19.206:5000",
+  baseURL: "http://192.168.1.105:5000",
   headers: {
     "Content-Type": "application/json",
   },
