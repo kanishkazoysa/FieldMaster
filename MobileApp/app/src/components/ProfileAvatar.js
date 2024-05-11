3a8a1feafcf560238899e59bb1db7eaec2121303
kanishkazoysa <125685188+kanishkazoysa@users.noreply.github.com>
2024-05-12 00:42:27 +0530

    FM | Edit profileAvatar

--- MobileApp/app/src/components/ProfileAvatar.js
+++ MobileApp/app/src/components/ProfileAvatar.js
@@ -3,12 +3,13 @@ import { TouchableOpacity, View } from "react-native";
 import { Avatar } from "react-native-paper";
 import { responsiveFontSize } from "react-native-responsive-dimensions";
 
-const ProfileAvatar = ({ userData, onPress, textSize }) => {
+const ProfileAvatar = ({ userData, onPress, textSize , image }) => {
+  const serverUrl = "http://192.168.1.102:5000"; // Replace with your server URL
+
   const getEmailColor = (email) => {
     if (!email) {
       return "#000000"; // Return a default color if email is undefined
     }
-
     let hash = 0;
     for (let i = 0; i < email.length; i++) {
       hash = email.charCodeAt(i) + ((hash << 5) - hash);
@@ -20,18 +21,23 @@ const ProfileAvatar = ({ userData, onPress, textSize }) => {
     }
     return color;
   };
+
   return (
     <View>
       {userData ? (
-        userData.imageUrl ? (
+        image || userData.imageUrl ? (
           <Avatar.Image
             size={responsiveFontSize(textSize)}
-            source={{ uri: userData.imageUrl }}
+            source={{
+              uri: image || `${serverUrl}/${userData.imageUrl}`, // Use the new image if it exists, otherwise use the existing one
+            }}
           />
         ) : (
           <Avatar.Text
             size={responsiveFontSize(textSize)}
-            label={`${(userData.fname ? userData.fname[0] : "").toUpperCase()}${(userData.lname ? userData.lname[0] : "").toUpperCase()}`}
+            label={`${(userData.fname ? userData.fname[0] : "").toUpperCase()}${
+              (userData.lname ? userData.lname[0] : "").toUpperCase()
+            }`}
             backgroundColor={getEmailColor(userData.email)}
           />
         )
