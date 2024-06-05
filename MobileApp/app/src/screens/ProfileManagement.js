77ed897c74e27649fd1f24466958539fa4501473
kanishkazoysa <125685188+kanishkazoysa@users.noreply.github.com>
2024-06-05 13:51:40 +0530

    change profilemange styles

--- MobileApp/app/src/screens/ProfileManagement.js
+++ MobileApp/app/src/screens/ProfileManagement.js
@@ -6,7 +6,7 @@ import {
   TouchableOpacity,
   ScrollView,
 } from "react-native";
-import { Appbar, TextInput, Button,  } from "react-native-paper";
+import { Appbar, TextInput, Button } from "react-native-paper";
 import {
   responsiveHeight,
   responsiveWidth,
@@ -17,10 +17,10 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useNavigation } from "@react-navigation/native";
 import ProfileAvatar from "../components/ProfileAvatar";
 import * as ImagePicker from "expo-image-picker";
-import Fontisto from '@expo/vector-icons/Fontisto';
+import Fontisto from "@expo/vector-icons/Fontisto";
 import { Alert } from "react-native";
 import AxiosInstance from "../AxiosInstance";
-import { ActivityIndicator } from 'react-native';
+import { ActivityIndicator } from "react-native";
 
 const ProfileManagement = () => {
   const [user, setUser] = useState({});
@@ -101,149 +101,177 @@ const ProfileManagement = () => {
         if (err.response.status === 400) {
           Alert.alert("Error", err.response.data.error);
           return;
-        }else{
+        } else {
           Alert.alert("Error", "An error occurred while sending OTP");
         }
-      
       });
   };
 
   const handleConfirm = async () => {
-  setLoading(true);
-  const token = await AsyncStorage.getItem("token");
-  let localUri = image;
-  let filename;
+    setLoading(true);
+    const token = await AsyncStorage.getItem("token");
+    let localUri = image;
+    let filename;
 
-  // Prepare the data
-  let formData = new FormData();
-  formData.append('user', JSON.stringify(user));
+    // Prepare the data
+    let formData = new FormData();
+    formData.append("user", JSON.stringify(user));
 
-  // Check if image is not null
-  if (localUri) {
-    filename = localUri.split('/').pop();
+    // Check if image is not null
+    if (localUri) {
+      filename = localUri.split("/").pop();
 
-    // Infer the type of the image
-    let match = /\.(\w+)$/.exec(filename);
-    let type = match ? `image/${match[1]}` : `image`;
+      // Infer the type of the image
+      let match = /\.(\w+)$/.exec(filename);
+      let type = match ? `image/${match[1]}` : `image`;
 
-    formData.append('photo', { uri: localUri, name: filename, type });
-  }
+      formData.append("photo", { uri: localUri, name: filename, type });
+    }
 
-  try {
-    const response = AxiosInstance.post(
-      "/api/users/updateProfile",
-      formData,
-      { headers: {'Content-Type': 'multipart/form-data', } }
-    );
-    navigation.navigate('Home');
-  } catch (error) {
-    console.log(error);
-    alert("An error occurred");
-  }
-};
+    try {
+      const response = AxiosInstance.post(
+        "/api/users/updateProfile",
+        formData,
+        { headers: { "Content-Type": "multipart/form-data" } }
+      );
+      setTimeout(() => {
+        setLoading(false);
+        navigation.navigate("Home");
+      }, 2000);
+    } catch (error) {
+      console.log(error);
+      alert("An error occurred");
+    }
+  };
   return (
     <View style={{ flex: 1 }}>
-    {loading && (
-      <View style={{
-        position: 'absolute',
-        left: 0,
-        right: 0,
-        top: 0,
-        bottom: 0,
-        backgroundColor: 'rgba(0,0,0,0.5)',
-        justifyContent: 'center',
-        alignItems: 'center',
-        zIndex: 2
-      }}>
-        <ActivityIndicator size="large" color="#0000ff" />
-      </View>
-    )}
-    <View style={styles.container}>
-      <StatusBar barStyle="light-content" backgroundColor="#007BFF" />
-      <Appbar.Header style={styles.header}>
-        <Appbar.BackAction onPress={() => navigation.goBack()} color="white" />
-        <Appbar.Content style={styles.appbarText} title="Profile" color="white" titleStyle={{ fontSize: 20 }}  />
-      </Appbar.Header>
-
-      <ScrollView>
-      <View style={styles.section1}>
-        <TouchableOpacity onPress={handlePressAvatar}>
-        <View style={{borderWidth: 3, borderColor: '#007BFF', borderRadius: 150, padding: 4.5}}>
-        <ProfileAvatar userData={user} textSize={16} image={image} />
-      </View>
-          <Fontisto style={styles.cameraIcon} name="camera" size={responsiveFontSize(2.4)} color="#007BFF" />
-        </TouchableOpacity>
-        <Text style={styles.avtarTxt}>
-          {user.fname} {user.lname}
-          </Text>
-          <Text>
-          {user.email}
-          </Text>
-      </View>
-
-      <View style={styles.section2}>
-        <Text style={styles.text1}>Your Information</Text>
-        <View style={styles.inputContainer}>
-          <TextInput
-            label="First Name"
-            mode="outlined"
-            outlineColor="#d9d7d2"
-            activeOutlineColor="#007BFF"
-            style={styles.inputField}
-            theme={{ roundness: 10 }}
-            value={user.fname}
-            onChangeText={(text) => setUser({ ...user, fname: text })}
-          />
-          <TextInput
-            label="Last Name"
-            mode="outlined"
-            outlineColor="#d9d7d2"
-            activeOutlineColor="#007BFF"
-            style={styles.inputField}
-            theme={{ roundness: 10 }}
-            value={user.lname}
-            onChangeText={(text) => setUser({ ...user, lname: text })}
-          />
-          <TextInput
-            label="Email"
-            mode="outlined"
-            outlineColor="#d9d7d2"
-            activeOutlineColor="#007BFF"
-            style={styles.inputField}
-            theme={{ roundness: 10 }}
-            value={user.email}
-            onChangeText={(text) => setUser({ ...user, email: text })}
-          />
+      {loading && (
+        <View
+          style={{
+            position: "absolute",
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
+            backgroundColor: "rgba(0,0,0,0.5)",
+            justifyContent: "center",
+            alignItems: "center",
+            zIndex: 2,
+          }}
+        >
+          <ActivityIndicator size="large" color="#0000ff" />
         </View>
-        <Button
-          onPress={handleChangePassword}
-          mode="outlined"
-          textColor="#007BFF"
-          loading={loading}
-          style={styles.button1}
-        >
-          Change Password
-        </Button>
-        <Button
-          onPress={handleConfirm}
-          mode="contained"
-          loading={loading}
-          style={styles.button}
-        >
-          Update
-        </Button>
+      )}
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content" backgroundColor="#007BFF" />
+        <Appbar.Header style={styles.header}>
+          <Appbar.BackAction
+            onPress={() => navigation.goBack()}
+            color="white"
+          />
+          <Appbar.Content
+            style={styles.appbarText}
+            title="Profile"
+            color="white"
+            titleStyle={{ fontSize: responsiveFontSize(2.5) }}
+          />
+        </Appbar.Header>
+
+        <ScrollView>
+          <View style={styles.section1}>
+            <TouchableOpacity onPress={handlePressAvatar}>
+              <View
+                style={{
+                  borderWidth: 3,
+                  borderColor: "#007BFF",
+                  borderRadius: 150,
+                  padding: 4.5,
+                }}
+              >
+                <ProfileAvatar
+                  userData={user}
+                  textSize={responsiveFontSize(2)}
+                  image={image}
+                />
+              </View>
+              <Fontisto
+                style={styles.cameraIcon}
+                name="camera"
+                size={responsiveFontSize(2.4)}
+                color="#007BFF"
+              />
+            </TouchableOpacity>
+            <Text style={styles.avtarTxt}>
+              {user.fname} {user.lname}
+            </Text>
+            <Text>{user.email}</Text>
+          </View>
+
+          <View style={styles.section2}>
+            <Text style={styles.text1}>Your Information</Text>
+            <View style={styles.inputContainer}>
+              <TextInput
+                label="First Name"
+                mode="outlined"
+                outlineColor="#d9d7d2"
+                activeOutlineColor="#007BFF"
+                style={styles.inputField}
+                theme={{ roundness: 10 }}
+                value={user.fname}
+                onChangeText={(text) => setUser({ ...user, fname: text })}
+              />
+              <TextInput
+                label="Last Name"
+                mode="outlined"
+                outlineColor="#d9d7d2"
+                activeOutlineColor="#007BFF"
+                style={styles.inputField}
+                theme={{ roundness: 10 }}
+                value={user.lname}
+                onChangeText={(text) => setUser({ ...user, lname: text })}
+              />
+              <TextInput
+                label="Email"
+                mode="outlined"
+                outlineColor="#d9d7d2"
+                activeOutlineColor="#007BFF"
+                style={styles.inputField}
+                theme={{ roundness: 10 }}
+                value={user.email}
+                onChangeText={(text) => setUser({ ...user, email: text })}
+              />
+            </View>
+           
+            <Button
+              onPress={handleChangePassword}
+              mode="contained"
+              loading={loading}
+              style={styles.button1}
+            >
+            <Text>
+            Change Password
+            </Text>
+            </Button>
+            <Button
+              onPress={handleConfirm}
+              mode="contained"
+              loading={loading}
+              style={styles.button}
+            >
+              Update
+            </Button>
+          </View>
+        </ScrollView>
       </View>
-      </ScrollView>
-    </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   header: {
-    height: 50,
+    height: responsiveHeight(6),
     backgroundColor: "#007BFF",
-zIndex:  1,
+    zIndex: 1,
     ...Platform.select({
       android: {
         marginTop: StatusBar.currentHeight,
@@ -288,11 +316,11 @@ zIndex:  1,
   button1: {
     marginTop: responsiveHeight(1),
     width: responsiveWidth(82),
-    height: responsiveHeight(5.5),
+    height: responsiveHeight(5.7),
     alignSelf: "center",
-    borderRadius:10,
-    borderWidth:1.7,
-    borderColor:"#007BFF"
+    justifyContent: "center",
+    borderRadius: 10,
+    borderColor: "#007BFF",
   },
   button: {
     marginTop: responsiveHeight(5),
@@ -301,10 +329,9 @@ zIndex:  1,
     padding: responsiveHeight(0),
     alignSelf: "center",
   },
-  cameraIcon:
-  { 
-    top: responsiveHeight(-2.7),
-    right: responsiveWidth(-27),
+  cameraIcon: {
+    top: responsiveHeight(-2.8),
+    right: responsiveWidth(-28),
   },
   avtarTxt: {
     fontSize: responsiveFontSize(3),
