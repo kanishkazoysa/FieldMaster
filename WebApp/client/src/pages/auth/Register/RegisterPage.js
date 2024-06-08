48a292ca934907f7e36851668597fd5fd455defd
kanishkazoysa <125685188+kanishkazoysa@users.noreply.github.com>
2024-06-09 01:16:31 +0530

    FM | edit register page

--- WebApp/client/src/pages/auth/Register/RegisterPage.js
+++ WebApp/client/src/pages/auth/Register/RegisterPage.js
@@ -3,7 +3,6 @@ import React from 'react'
 const RegisterPage = () => {
   return (
     <div>
-      <p>gdiucsic nb sdcvy souycvsd9u c9sdyfc8o sdyc89s7dyc s c8ysc8osaycso9cy9 cyaxc</p>
     </div>
   )
 }
