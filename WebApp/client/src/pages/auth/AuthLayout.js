5a2512e0257132638fbdb6d60afe6f461e4e2dc3
kanishkazoysa <125685188+kanishkazoysa@users.noreply.github.com>
2024-06-09 01:16:52 +0530

    FM | edit

--- WebApp/client/src/pages/auth/AuthLayout.js
+++ WebApp/client/src/pages/auth/AuthLayout.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { Carousel } from 'antd';
+import './AuthLayout.css';
+import sideImg from './img1.png'; // Import your image
+
+const contentStyle = {
+  height: '92vh',
+  color: '#fff',
+  lineHeight: '160px',
+  textAlign: 'center',
+  background: '#fff',
+};
 
 const AuthLayout = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const isAuthenticated = false;
 
-export default AuthLayout
+  return (
+    <div className="background">
+      <div className="carousel-container">
+        <Carousel autoplay>
+          <div>
+            <img src={sideImg} alt="Slide 1" style={contentStyle} />
+          </div>
+          <div>
+            <img src={sideImg} alt="Slide 2" style={contentStyle} />
+          </div>
+          <div>
+            <img src={sideImg} alt="Slide 3" style={contentStyle} />
+          </div>
+          <div>
+            <img src={sideImg} alt="Slide 4" style={contentStyle} />
+          </div>
+        </Carousel>
+      </div>
+    </div>
+  );
+};
+
+export default AuthLayout;
