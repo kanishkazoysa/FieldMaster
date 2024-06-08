1d7c60fe25e9ae3b949985fbdea2df8035d540c9
kanishkazoysa <125685188+kanishkazoysa@users.noreply.github.com>
2024-06-08 16:54:19 +0530

    FM | create new register page

---
+++ WebApp/client/src/pages/auth/Register/RegisterPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RegisterPage = () => {
+  return (
+    <div>
+      <p>gdiucsic nb sdcvy souycvsd9u c9sdyfc8o sdyc89s7dyc s c8ysc8osaycso9cy9 cyaxc</p>
+    </div>
+  )
+}
+
+export default RegisterPage
