633cbfd721c426b079a045ebf3a9a2f34d04fc1a
kanishkazoysa <125685188+kanishkazoysa@users.noreply.github.com>
2024-06-18 15:50:53 +0530

    edit profile modal

--- WebApp/client/src/components/profileManage/ProfileModal.js
+++ WebApp/client/src/components/profileManage/ProfileModal.js
@@ -1,5 +1,7 @@
+import { Avatar } from "antd";
 import React, { useEffect } from "react";
-import Avatar from "./Avatar";
+import { Button} from 'antd';
+import { Divider } from "antd";
 
 const ProfileModal = ({ isOpen, onRequestClose }) => {
   useEffect(() => {
@@ -19,19 +21,18 @@ const ProfileModal = ({ isOpen, onRequestClose }) => {
           &times;
         </button>
         <div style={styles.content}>
+        <p>kanishkazoysa1234@gmail.com</p>
+        <Divider />
           <Avatar
-            style={{
-              backgroundColor: "#87d068",
-              position: "absolute",
-              left: "50%",
-              top: "50%",
-              transform: "translate(-50%, -50%)", // Center the avatar
-              width: "100px", // Adjust these values to change the size of the avatar
-              height: "100px", // Adjust these values to change the size of the avatar
-            }}
+          size={150}
+            style={styles.avatar}
           />
+          <h4 style={styles.hellotxt}>Hi, Kanishka!</h4>
+          <Button type="primary" style={{marginTop: "40px",padding:"18px",width:"70%",borderRadius:20}}>Manage Your Profile</Button>
+          <Button  style={{marginTop: "20px",padding:"18px",width:"70%",borderRadius:20, marginBottom:"10px"}}>Sign Out From Your Account</Button>
+          <Divider />
+          <p style={styles.bottomtxt}>Privacy Policy . Terms of Services</p>
         </div>
-        
       </div>
     </div>
   );
@@ -53,19 +54,29 @@ const styles = {
     opacity: 1,
     transition: "opacity 3s ease-in-out",
   },
+  hellotxt: {
+    marginTop: "20px",
+    fontSize: "1.5rem",
+  },
+  bottomtxt: {
+    position: "absolute",
+    bottom:0,
+    fontSize: "0.8rem",
+  },
   content: {
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
     flexDirection: "column",
   },
+  avatar: {
+  },
   modal: {
     background: "white",
     borderRadius: "10px",
     boxShadow: "0 0 10px rgba(0, 0, 0, 0.25)",
-    width: "80%",
+    width: "25%",
     height: "80%",
-    maxWidth: "500px",
     padding: "20px",
     position: "relative",
     transform: "scale(1)",
