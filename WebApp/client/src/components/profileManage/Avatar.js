170ecf97469d5380c2174cb496d357c78c3df92d
kanishkazoysa <125685188+kanishkazoysa@users.noreply.github.com>
2024-06-18 15:50:39 +0530

    change styles

--- WebApp/client/src/components/profileManage/Avatar.js
+++ WebApp/client/src/components/profileManage/Avatar.js
@@ -2,15 +2,12 @@ import React from 'react';
 import { UserOutlined } from '@ant-design/icons';
 import { Avatar, Space } from 'antd';
 const url = 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg';
-const App = ({onClick}) => (
+const App = ({onClick }) => (
   <Space size={16} wrap>
     <Avatar
         onClick={onClick}
       style={{
         backgroundColor: '#87d068',
-        position: 'absolute',
-        right: '0.5%',
-        top: '2.5%',
       }}
       icon={<UserOutlined />}
     />
