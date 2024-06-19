ac8067b03a7d9b63d2940df02c1f52d4388464e7
Chathura Abeygunawardhana <chathuraabeygunawardhana@gmail.com>
2024-06-19 19:46:38 +0530

    Revert "FM | created the TemplateDetails component"
    This reverts commit 3a01d1862e3de5c20fd2027006ece8027b29e9c7.

--- WebApp/client/src/components/SavedTemplates/SavedTemplatesWeb.js
+++ WebApp/client/src/components/SavedTemplates/SavedTemplatesWeb.js
@@ -1,58 +1,39 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { MdArrowBack } from 'react-icons/md';
 import '../SavedTemplates/SavedTemplatesWeb.css';
 import Card from './Card';
 import { FaSearch } from 'react-icons/fa';
-import templatesList from './TemplatesList.js';
-import TemplateDetails from './TemplateDetails.js';
+import templatesList from './TemplatesList.js'; // import the templatesList
 
 const SavedTemplatesWeb = ({ onBackToSidebar }) => {
-  const [selectedTemplate, setSelectedTemplate] = useState(null);
-
-  const handleCardClick = (template) => {
-    setSelectedTemplate(template);
-  };
-
-  const handleBack = () => {
-    setSelectedTemplate(null);
-  };
-
   return (
     <>
       <div className='innerDiv'>
-        {!selectedTemplate && (
-          <MdArrowBack onClick={onBackToSidebar} className='backBtn' />
-        )}
+        <MdArrowBack onClick={onBackToSidebar} className='backBtn' />
         <div className='headingDiv'>
-          {selectedTemplate ? (
-            <TemplateDetails template={selectedTemplate} onBack={handleBack} />
-          ) : (
-            <>
-              <div className='search-container'>
-                <input
-                  type='text'
-                  className='search-bar'
-                  placeholder='Search your templates'
-                />
-                <FaSearch className='search-icon' />
-              </div>
+          <div className='search-container'>
+            <input
+              type='text'
+              className='search-bar'
+              placeholder='Search your templates'
+            />
+            <FaSearch className='search-icon' />
+          </div>
 
-              <div className='cardsDiv'>
-                {templatesList.map((template, index) => (
-                  <Card
-                    key={index}
-                    templateName={template.templateName}
-                    location={template.location}
-                    date={template.date}
-                    onClick={() => handleCardClick(template)}
-                  />
-                ))}
-              </div>
-            </>
-          )}
+          <div className='cardsDiv'>
+            {templatesList.map((template, index) => (
+              <Card
+                key={index}
+                templateName={template.templateName}
+                location={template.location}
+                date={template.date}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </>
   );
 };
+
 export default SavedTemplatesWeb;
