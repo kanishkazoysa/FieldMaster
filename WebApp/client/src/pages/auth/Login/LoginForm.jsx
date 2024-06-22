598af82bcee2e3ff16c5b32302e7f22fff38d6cb
Kaveen Dinethma <dezoysalklk.21@uom.lk>
2024-06-22 15:48:24 +0530

    Reapply "Merge branch 'develop' into kanishkaOne"
    This reverts commit b788f6ac3fa2448137b7c4bb9bc4f9358b9824d8.

--- WebApp/client/src/pages/auth/Login/LoginForm.jsx
+++ WebApp/client/src/pages/auth/Login/LoginForm.jsx
@@ -1,101 +1,111 @@
 import React from "react";
-import { Form, Input, Button } from "antd";
-import { Link } from "react-router-dom";
+import { Form, Input, Button, message } from "antd";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import "../Register/RegisterStyle.css";
 
+
+
+
 const LoginForm = () => {
-    const onFinish = (values) => {
-        const { email, password } = values;
+  const navigate = useNavigate(); // Use useNavigate hook
 
-        axios
-            .post("/api/users/login", {
-                email,
-                password,
-            })
-            .then(() => {
-                window.location.href = "/Home";
-            })
-            .catch((error) => {
-                console.error("There was an error login !", error);
-            });
-    };
+  const onFinish = (values) => {
+    const { email, password } = values;
+    
+    axios
+      .post("/api/users/login", {
+        email,
+        password,
+      })
+      .then((response) => {
+        //get token and store it on local storage
+        const token= response.data.token; 
+        localStorage.setItem("UserToken", token);
 
-    return (
-        <div className="register-form-container">
-            <div className="form-header">
-                <h3>Sign In</h3>
-                <p>Welcome back! Please enter your details</p>
-            </div>
-            <br />
+        // Use navigate for redirection after successful login
+        navigate('/Home', { replace: true, state: { loginSuccess: true } });
+        // No need to show message here, it will be handled in Home component
+      })
+      .catch((error) => {
+        if (error.response && error.response.data && error.response.data.error) {
+          // Displaying the error message using antd message component
+          message.error(error.response.data.error);
+        } else {
+          // Fallback error message if the expected structure is not found
+          message.error("An unexpected error occurred. Please try again.");
+        }
+        console.error("There was an error during login!", error);
+      });
+  };
 
-            <Form
-                name="register"
-                initialValues={{
-                    remember: true,
-                }}
-                onFinish={onFinish}
-                autoComplete="off"
-            >
-                <label>Email</label>
-                <Form.Item
-                    hasFeedback
-                    name="email"
-                    rules={[
-                        {
-                            required: true,
-                            message: "Please input your email!",
-                        },
-                        {
-                            type: "email",
-                            message: "Please enter a valid email!",
-                        },
-                    ]}
-                >
-                    <Input placeholder="Email" />
-                </Form.Item>
-                <label>Password</label>
-                <Form.Item
-                    hasFeedback
-                    name="password"
-                    rules={[
-                        {
-                            required: true,
-                            message: "Please input your password!",
-                        },
-                    ]}
-                >
-                    <Input.Password placeholder="Password" />
-                </Form.Item>
+  return (
+    <div className="register-form-container">
+      <div className="form-header">
+        <h3>Sign In</h3>
+        <p>Welcome back! Please enter your details</p>
+      </div>
+      <br />
 
-                <div className="container-sign-in">
-                    <div className="csi-div">
-                        <input type="checkbox" name="remember" />{" "}
-                        <label>Remember Me</label>
-                    </div>
-                    <div className="csi-div">
-                        <Link to="/forgot-password">Forgot Password?</Link>
-                    </div>
-                </div>
-                <br />
+      <Form
+        name="register"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        autoComplete="off"
+      >
+        <label>Email</label>
+        <Form.Item
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: "Please input your email!",
+            },
+            {
+              type: "email",
+              message: "Please enter a valid email!",
+            },
+          ]}
+        >
+          <Input placeholder="Email" />
+        </Form.Item>
+        <label>Password</label>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
+            },
+          ]}
+        >
+          <Input.Password placeholder="Password" />
+        </Form.Item>
 
-                <Form.Item>
-                    <Button
-                        type="primary"
-                        htmlType="submit"
-                        className="register-button"
-                    >
-                        Sign In
-                    </Button>
-                </Form.Item>
-
-                <div className="already-have-account">
-                    Don't have an account yet?{" "}
-                    <Link to="/register">signup</Link>
-                </div>
-            </Form>
+        <div className="container-sign-in">
+          <div className="csi-div">
+            <input type="checkbox" name="remember" /> <label>Remember Me</label>
+          </div>
+          <div className="csi-div">
+            <Link to="/forgot-password">Forgot Password?</Link>
+          </div>
         </div>
-    );
+        <br />
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit" className="register-button">
+            Sign In
+          </Button>
+        </Form.Item>
+
+        <div className="already-have-account">
+          Don't have an account yet? <Link to="/register">signup</Link>
+        </div>
+      </Form>
+    </div>
+  );
 };
 
 export default LoginForm;
