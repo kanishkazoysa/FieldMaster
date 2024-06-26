56311b8f4aa9d6127a10b00fbe95519fb54785d5
Chathura Abeygunawardhana <chathuraabeygunawardhana@gmail.com>
2024-06-26 11:22:15 +0530

    FM | added edit and delete icons to the templates
    Edit icon and delete icons were added to the Card.js file , which contains the details of each template ,that's loaded from the database

--- WebApp/client/src/components/SavedTemplates/Card.js
+++ WebApp/client/src/components/SavedTemplates/Card.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './Card.css';
+import { RiDeleteBin6Line } from 'react-icons/ri';
+import { BiEdit } from 'react-icons/bi';
 
 const Card = ({ templateName, location, date, onClick }) => {
   return (
     <div className='card-container' onClick={onClick}>
+      <BiEdit className='edit-icon' />
+      <RiDeleteBin6Line className='delete-icon' />
       <div className='card-flex'>
         <div className='card-image-container'>
           <img
