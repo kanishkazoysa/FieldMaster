dbe1d61bfe57faa40c0f01809015196d526749a9
Shenali Gamage <87947238+Nethmi-Shenali@users.noreply.github.com>
2024-06-28 22:11:27 +0530

    FM | perch to two decimals

--- WebApp/client/src/components/Calculator/calculatorSelect.js
+++ WebApp/client/src/components/Calculator/calculatorSelect.js
@@ -10,7 +10,7 @@ import FenceManual from '../Fence Manual/Fence/fenceManual';
 const convertAreaToSqMeters = (area, unit) => {
     const conversionRates = {
         "Acres": 4046.86,  // 1 Acre = 4046.86 sq meters
-        "Perch": 25.2929,  // 1 Perch = 25.2929 sq meters
+        "Perch": 25.29,  // 1 Perch = 25.29 sq meters
         "m²": 1            // 1 sq meter = 1 sq meter
     };
 
