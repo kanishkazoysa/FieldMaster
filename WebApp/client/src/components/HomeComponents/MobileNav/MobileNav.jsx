22487935ec5c89b04cb938a99fd4fe522159006a
Shenali Gamage <87947238+Nethmi-Shenali@users.noreply.github.com>
2024-07-01 21:19:25 +0530

    FM | mobile nav formatted

--- WebApp/client/src/components/HomeComponents/MobileNav/MobileNav.jsx
+++ WebApp/client/src/components/HomeComponents/MobileNav/MobileNav.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./MobileNav.css";
+import logo from "../../../images/logo.png"
 import GetStarted from "../../GetStarted"; 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-gvf7LaAPdVMTIn4IuZbRirUFLBAmT/7/3gHvZuOWv7dM0HZ5zmZbZIbyibGLazmu+q3UqGzKqz8n0dFwXOoY7w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 const MobileNav = ({ isOpen, toggleMenu }) => {
@@ -19,8 +20,8 @@ const MobileNav = ({ isOpen, toggleMenu }) => {
         onClick={toggleMenu}
       >
         <div className="mobile-menu-container">
-          <h1 className="styled">FIELDMASTER</h1>
-
+        <div className="logo"><img className="image" src={logo} alt="FIELDMASTER" />
+        </div>
           <ul>
             <li>
               <a onClick={() => handleScroll("hero")} className="menu-item">
