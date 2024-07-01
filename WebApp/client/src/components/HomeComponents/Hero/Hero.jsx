23fb2e6df8e8c525ea67acde98d577dc5f8f47fe
Shenali Gamage <87947238+Nethmi-Shenali@users.noreply.github.com>
2024-07-01 14:25:58 +0530

    FM | hero responsive done

--- WebApp/client/src/components/HomeComponents/Hero/Hero.jsx
+++ WebApp/client/src/components/HomeComponents/Hero/Hero.jsx
@@ -8,40 +8,30 @@ const Hero = () => {
   const toggleModal = () => {
     setIsModalOpen(!isModalOpen);
   };
+
   return (
     <section id="hero" className="hero-container">
-      <div>
-        
-      </div>
       <div className="hero-content">
         <div className="hero-text-content">
           <h4>The Best Platform for Accurate measurements of your land</h4>
-          <p>Trusted by beginners,marketers & 
-        professionals ; Built with usability and performance in mind.</p>
-        <div className="hero-btn">
-        <button className="contact-btn" onClick={toggleModal}>
+          <p>Trusted by beginners, marketers & professionals; Built with usability and performance in mind.</p>
+          <div className="hero-btn">
+            <button className="contact-btn" onClick={toggleModal}>
               Download Now
             </button>
+          </div>
         </div>
-        </div>
-        
       </div>
 
       <div className="hero-video">
-        
-          <video width="100%" height="100%"  autoPlay loop muted>
+        <video width="100%" height="100%" autoPlay loop muted>
           <source src={video1} type="video/mp4" />
-          </video>
-        
-
-      
+        </video>
       </div>
-      {isModalOpen && <GetStarted toggleModal={toggleModal}/>} {/* Renamed imported component */}
-     
+
+      {isModalOpen && <GetStarted toggleModal={toggleModal} />} {/* Renamed imported component */}
     </section>
-    
   );
 };
-
 
 export default Hero;
