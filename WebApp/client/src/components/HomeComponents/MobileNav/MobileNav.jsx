468f1296e074769ebc1d5e9bf3bc8f75a605adbe
Shenali Gamage <87947238+Nethmi-Shenali@users.noreply.github.com>
2024-07-01 15:43:21 +0530

    FM | mobile nav get started button fixed

--- WebApp/client/src/components/HomeComponents/MobileNav/MobileNav.jsx
+++ WebApp/client/src/components/HomeComponents/MobileNav/MobileNav.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./MobileNav.css";
+import GetStarted from "../../GetStarted"; 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-gvf7LaAPdVMTIn4IuZbRirUFLBAmT/7/3gHvZuOWv7dM0HZ5zmZbZIbyibGLazmu+q3UqGzKqz8n0dFwXOoY7w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -9,7 +10,10 @@ const MobileNav = ({ isOpen, toggleMenu }) => {
     if(isOpen) toggleMenu();
     document.getElementById(sectionId).scrollIntoView({ behavior: "smooth" });
   };
-
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
   return (
     <>
       <div
@@ -48,9 +52,10 @@ const MobileNav = ({ isOpen, toggleMenu }) => {
               </a>
             </li>
 
-            <button className="contact-btn" onClick={() => {}}>
+            <button className="contact-btn" onClick={toggleModal}>
               Get Started
             </button>
+            {isModalOpen && <GetStarted toggleModal={toggleModal}/>}
           </ul>
         </div>
       </div>
