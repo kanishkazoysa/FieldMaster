0426fa404e215466de27e90f6ae860b1069089e0
kanishkazoysa <125685188+kanishkazoysa@users.noreply.github.com>
2024-07-09 01:40:43 +0530

    change styles

--- WebApp/client/src/pages/auth/AuthLayout.js
+++ WebApp/client/src/pages/auth/AuthLayout.js
@@ -1,12 +1,13 @@
 import { Carousel } from 'antd';
 import React from 'react';
-import sideimg1 from '../../images/sideimg1.png';
-import sideimg2 from '../../images/sideimg2.png';
-import sideimg3 from '../../images/sideimg3.png';
+import slideimg1 from '../../images/slideimg1.png';
+import slideimg2 from '../../images/slideimg2.png';
+import slideimg3 from '../../images/slideimg3.png';
 import './AuthLayout.css';
 
 const contentStyle = {
   height: '97vh',
+  width: '100%',
   color: '#fff',
   lineHeight: '160px',
   textAlign: 'center',
@@ -19,13 +20,13 @@ const AuthLayout = ({ children }) => {
       <div className="carousel-container">
         <Carousel autoplay>
           <div>
-            <img src={sideimg1} alt="Slide 1" style={contentStyle} />
+            <img src={slideimg1} alt="Slide 1" style={contentStyle} />
           </div>
           <div>
-            <img src={sideimg2} alt="Slide 2" style={contentStyle} />
+            <img src={slideimg2} alt="Slide 2" style={contentStyle} />
           </div>
           <div>
-            <img src={sideimg3} alt="Slide 3" style={contentStyle} />
+            <img src={slideimg3} alt="Slide 3" style={contentStyle} />
           </div>
         </Carousel>
         {children}
