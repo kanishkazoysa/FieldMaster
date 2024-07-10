f715eb7d909502059e33cf9d8be4a96b3a073fe1
Chathura Abeygunawardhana <chathuraabeygunawardhana@gmail.com>
2024-07-10 08:08:14 +0530

    FM | added loading to images in SavedTemplatesWeb

--- WebApp/client/src/components/SavedTemplates/Card.js
+++ WebApp/client/src/components/SavedTemplates/Card.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import './Card.css';
-import { RiDeleteBin6Line } from 'react-icons/ri';
-import { BiEdit } from 'react-icons/bi';
+import React, { useState } from "react";
+import "./Card.css";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { BiEdit } from "react-icons/bi";
 
 const Card = ({
   templateName,
@@ -12,6 +12,8 @@ const Card = ({
   onDelete,
   onEdit,
 }) => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
   const handleDelete = (e) => {
     e.stopPropagation();
     onDelete();
@@ -22,18 +24,28 @@ const Card = ({
     onEdit();
   };
 
+  const handleImageLoad = () => {
+    setImageLoaded(true);
+  };
+
   return (
-    <div className='card-container' onClick={onClick}>
-      <BiEdit className='edit-icon' onClick={handleEdit} />
-      <RiDeleteBin6Line className='delete-icon' onClick={handleDelete} />
-      <div className='card-flex'>
-        <div className='card-image-container'>
-          <img src={imageUrl} className='card-image' alt='mapImg' />
+    <div className="card-container" onClick={onClick}>
+      <BiEdit className="edit-icon" onClick={handleEdit} />
+      <RiDeleteBin6Line className="delete-icon" onClick={handleDelete} />
+      <div className="card-flex">
+        <div className="card-image-container">
+          {!imageLoaded && <div className="image-loader"></div>}
+          <img
+            src={imageUrl}
+            className={`card-image ${imageLoaded ? "loaded" : ""}`}
+            alt="mapImg"
+            onLoad={handleImageLoad}
+          />
         </div>
-        <div className='card-content'>
-          <div className='templateName'>{templateName}</div>
-          <p className='templateLocation'>Location : {location}</p>
-          <p className='dateText'>Date : {date}</p>
+        <div className="card-content">
+          <div className="templateName">{templateName}</div>
+          <p className="templateLocation">Location : {location}</p>
+          <p className="dateText">Date : {date}</p>
         </div>
       </div>
     </div>
