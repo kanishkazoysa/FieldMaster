b23cad6e3e3cb3b1025ebcfa35ef15adafddb65f
kanishkazoysa <125685188+kanishkazoysa@users.noreply.github.com>
2024-07-16 01:42:50 +0530

    edit api with auth

--- WebApp/client/src/pages/auth/Login/LoginForm.jsx
+++ WebApp/client/src/pages/auth/Login/LoginForm.jsx
@@ -10,7 +10,7 @@ const LoginForm = () => {
   const onFinish = (values) => {
     const { email, password } = values;
     
-    AxiosInstance.post("/api/users/login", {
+    AxiosInstance.post("/apiauth/users/login", {
         email,
         password,
       })
