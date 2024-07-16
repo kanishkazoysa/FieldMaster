f6c8bbc5b57a2ec231cc0ee8832e46f7df23ea74
kanishkazoysa <125685188+kanishkazoysa@users.noreply.github.com>
2024-07-16 14:05:54 +0530

    Vercel Deploy

--- WebApp/Backend/server.js
+++ WebApp/Backend/server.js
@@ -25,8 +25,8 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use(
   cors({
-    origin: ["https://lpc-lipton.vercel.app/"],
-    methods: ["POST", "GET"],
+    origin: ["https://fieldmaster-sage.vercel.app/"],
+    methods: ["POST", "GET" , "PUT", "DELETE" , "PATCH" , "OPTIONS" , "HEAD" ],
     credentials: true,
   })
 );
