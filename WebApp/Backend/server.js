93a5132517778b0fd25b3082ee30d2cad0035c24
kanishkazoysa <125685188+kanishkazoysa@users.noreply.github.com>
2024-07-16 17:52:04 +0530

    Update Server

--- WebApp/Backend/server.js
+++ WebApp/Backend/server.js
@@ -27,7 +27,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ["https://field-master-frontend.vercel.app/", "http://localhost:3000"],
+    origin: ["https://field-master-frontend.vercel.app/"],
     methods: ["POST", "GET", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD"],
     credentials: true,
   })
