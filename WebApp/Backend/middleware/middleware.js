fb91ce6c8064ec37cba9d8c13714bbc5d81c411a
kanishkazoysa <125685188+kanishkazoysa@users.noreply.github.com>
2024-07-16 18:49:50 +0530

    Middleware update

--- WebApp/Backend/middleware/middleware.js
+++ WebApp/Backend/middleware/middleware.js
@@ -1,29 +1,41 @@
 const JWT = require("jsonwebtoken");
+
 module.exports = async (req, res, next) => {
   try {
-    console.log(req.headers,'auth')
+    console.log(req.headers, 'auth');
     if (!req.headers.authorization) {
       return res.status(401).send({
         success: false,
         message: "Auth Failed",
       });
     }
-    const token = req.headers.authorization;
+
+    // Extract token from authorization header
+    const authHeader = req.headers.authorization;
+    const token = authHeader.split(' ')[1]; // Assumes the format is "Bearer token"
+
+    if (!token) {
+      return res.status(401).send({
+        success: false,
+        message: "Auth Failed",
+      });
+    }
+
     JWT.verify(token, process.env.JWT_SECRET, (err, decode) => {
       console.log("decode", decode);
       if (err) {
-        return res.status(401).send({ 
+        return res.status(401).send({
           success: false,
           message: "Auth Failed",
           err,
         });
       } else {
-        req.userId= decode.id;
+        req.userId = decode.id;
         next();
       }
     });
   } catch (error) {
-    console.log(error,'no token');
+    console.log(error, 'no token');
     return res.status(401).send({
       success: false,
       error,
