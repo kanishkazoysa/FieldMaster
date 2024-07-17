423e427fd6233ff7acbcbeeecc7e3718f7f8eff6
Chathura Abeygunawardhana <chathuraabeygunawardhana@gmail.com>
2024-07-17 13:33:07 +0530

    FM | AxiosInstance changed

--- MobileApp/app/src/AxiosInstance.js
+++ MobileApp/app/src/AxiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const AxiosInstance = axios.create({
-  baseURL: "http://192.168.1.103:5000",
+  baseURL: "https://field-master-backen.vercel.app/",
   headers: {
     "Content-Type": "application/json",
   },
@@ -12,7 +12,7 @@ AxiosInstance.interceptors.request.use(
   async (config) => {
     const token = await AsyncStorage.getItem("token");
     if (token) {
-      config.headers.authorization = `${token}`;
+      config.headers.authorization = `Bearer ${token}`;
     }
     return config;
   },
