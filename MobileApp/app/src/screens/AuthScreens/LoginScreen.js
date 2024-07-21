d1ba44478d26f944dca4e6ecec15276916dda49e
kanishkazoysa <125685188+kanishkazoysa@users.noreply.github.com>
2024-07-21 20:11:18 +0530

    update app bar styles

--- MobileApp/app/src/screens/AuthScreens/LoginScreen.js
+++ MobileApp/app/src/screens/AuthScreens/LoginScreen.js
@@ -152,9 +152,9 @@ export default function LoginScreen() {
 
 const styles = StyleSheet.create({
   header: {
-    height: responsiveHeight(6.5),
+    height: responsiveHeight(3),
+    paddingBottom: responsiveHeight(3),
     backgroundColor: "#007BFF",
-
     ...Platform.select({
       android: {
         marginTop: StatusBar.currentHeight,
