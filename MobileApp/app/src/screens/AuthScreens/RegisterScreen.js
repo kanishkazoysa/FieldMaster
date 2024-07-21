ebff871376ed5e6e357afc8031f9feb9b0efa5eb
kanishkazoysa <125685188+kanishkazoysa@users.noreply.github.com>
2024-07-21 20:11:12 +0530

    update app bar styles

--- MobileApp/app/src/screens/AuthScreens/RegisterScreen.js
+++ MobileApp/app/src/screens/AuthScreens/RegisterScreen.js
@@ -239,9 +239,9 @@ export default function RegisterScreen() {
 
 const styles = StyleSheet.create({
   header: {
-    height: responsiveHeight(6.5),
+    height: responsiveHeight(3),
+    paddingBottom: responsiveHeight(3),
     backgroundColor: "#007BFF",
-
     ...Platform.select({
       android: {
         marginTop: StatusBar.currentHeight,
